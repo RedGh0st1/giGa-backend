f1372011c547de6423dbb0a8f1f3662f857fd54b
Lennie Nurse <lnursej@gmail.com>
2023-02-10 13:20:14 -0500

    "missing return value for create post"

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -34,6 +34,7 @@ router.get("/:id", async (req, res) => {
 router.post("/", async (req, res) => {
   try {
     const createdGame = await createGames(req.body)
+    res.status(200).json(createdGame)
   } catch (error) {
     res.status(500).json({ error: error })
   }
